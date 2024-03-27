e1982ed4724211862e02af19ddd85e316584372c
Alex Wang <iamalex11303@gmail.com>
2024-03-27 14:42:24 -0400

    responds with unix time

--- index.js
+++ index.js
@@ -30,3 +30,8 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+app.get("/api/:date", (req, res) => {
+  res.json({"unix": new Date(req.params.date).getTime()});
+});
